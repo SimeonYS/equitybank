54099b0ceca43f823b7cb83df6d93f8dee373781
simeon.simeonov <simeon.simeonov@adata.pro>
2021-04-06 13:58:25 +0300

    Done

---
+++ README.md
@@ -0,0 +1,9 @@
+URL: https://www.equitybank.com/your-business/blog
+
+Spider name: equitybank
+DB Schema:
+
+date
+title
+link
+content
---
+++ equitybank/spiders/spider.py
@@ -0,0 +1,32 @@
+import re
+import scrapy
+from scrapy.loader import ItemLoader
+from ..items import EequitybankItem
+from itemloaders.processors import TakeFirst
+
+pattern = r'(\xa0)?'
+
+class EequitybankSpider(scrapy.Spider):
+	name = 'equitybank'
+	start_urls = ['https://www.equitybank.com/your-business/blog']
+
+	def parse(self, response):
+		post_links = response.xpath('(//div[@class="blurb content"])[1]//a/@href[not (ancestor::p[@class="biggest"])]').getall()
+		yield from response.follow_all(post_links, self.parse_post)
+
+	def parse_post(self, response):
+		date = "Not in article"
+		title = response.xpath('//h1//text()').get()
+		content = response.xpath('(//div[@class="blurb content"])[1]//text()').getall()
+		content = [p.strip() for p in content if p.strip()]
+		content = re.sub(pattern, "",' '.join(content))
+
+		item = ItemLoader(item=EequitybankItem(), response=response)
+		item.default_output_processor = TakeFirst()
+
+		item.add_value('title', title)
+		item.add_value('link', response.url)
+		item.add_value('content', content)
+		item.add_value('date', date)
+
+		yield item.load_item()
